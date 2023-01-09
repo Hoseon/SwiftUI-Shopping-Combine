86bd2b6f5b70c59867626193778d08126b87a597
hoseon <chs1025@gmail.com>
2023-01-10 08:00:51 +0900

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by HoSeon Chu on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
